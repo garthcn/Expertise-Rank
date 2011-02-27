23db969cd39e6d848343c8b3c3ca5a4151cd1622
FlyClover <wjruoxue@gmail.com>
2011-02-26 16:42:10 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace Expertise_Rank
+{
+    class Program
+    {
+        static int g_count = 0;
+        static void Main(string[] args)
+        {
+            SqlConnection myConnection = new SqlConnection(GetConnectionString());
+
+            // Open database
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Database open error: " + e.ToString());
+            }
+
+            // start caculating expertise
+            calculate_expertise(myConnection);
+
+            // Close database
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("====================== Done ==========================");
+            Console.ReadKey();
+
+        }
+
+        static private string GetConnectionString()
+        {
+            return "user id=cfa18;server=localhost;" +
+                "Trusted_Connection=yes;database=Trustlet_2;connection timeout=30";
+        }
+
+        static private void calculate_expertise(SqlConnection connection)
+        {
+
+            
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("select * from [reviews-all]", connection);
+
+            /*=====================Calculate Expertise ==================*/
+
+            Dictionary<String, User> users = new Dictionary<String, User>();
+            myReader = myCommand.ExecuteReader();        
+            while (myReader.Read())
+            {
+                //Console.WriteLine(myReader["member_since"].ToString());
+                Insert(myReader, users);
+            }
+
+            //
+            foreach (KeyValuePair<String, User> kvp in users)
+            {
+                User u = kvp.Value;
+                u.CalculateExpertise();
+                //rank.Add(u);
+            }
+            myReader.Close();
+           
+
+
+            /*=====================Calculate Trustworthiness ==================*/
+
+            // Create web of trust
+            myCommand = new SqlCommand("select * from [trusts]", connection);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                String source_user = myReader["source_user"].ToString();
+                String target_user = myReader["target_user"].ToString();
+                if (users.ContainsKey(target_user))
+                    users[target_user].AddTrustBy(source_user);
+            }
+            myReader.Close();
+
+            // Calculate trustworthiness
+            //List<User> rank = new List<User>();
+            //TextWriter file = new StreamWriter("TrustBy.txt");
+            foreach (KeyValuePair<String, User> kvp in users)
+            {
+                User u = kvp.Value;
+                foreach (String category in User.CategoryName)
+                {
+                    foreach (String trusteeStr in u.TrustBy)
+                    {
+                        if (users.ContainsKey(trusteeStr))
+                        {
+                            User trustee = users[trusteeStr];
+                            if (trustee.Credibility[category].Expertise != 0.0d)
+                            {
+                                u.Credibility[category].TotalTrusteeExpertise +=
+                                    trustee.Credibility[category].Expertise;
+                                u.Credibility[category].TrusteeCount++;
+                            }
+                        }
+                    }
+                }
+
+                u.CalculateTrustwhorthiness();
+                InsertIntoTable(u, "Credibility", connection);
+                //rank.Add(u);
+                //file.WriteLine("{0} {1}", kvp.Key, kvp.Value.TrustBy.Count);
+            }
+            //rank.Sort(userComparator);
+            //WriteToFile( rank);
+            //file.Close();
+        }
+
+        private static int userTrComparator(User u1, User u2)
+        {
+            CredibilityInfo info1 = u1.GetCredibilityInfo("Movies");
+            CredibilityInfo info2 = u2.GetCredibilityInfo("Movies");
+            if (info1.Trustworthiness < info2.Trustworthiness)
+                return 1;
+            else if (info1.Trustworthiness > info2.Trustworthiness)
+                return -1;
+            else
+            {
+                if (info1.TrusteeCount > info2.TrusteeCount)
+                    return -1;
+                else if (info1.TrusteeCount < info2.TrusteeCount)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+
+        private static int userExpComparator(User u1, User u2)
+        {
+            CredibilityInfo info1 = u1.GetCredibilityInfo("Movies");
+            CredibilityInfo info2 = u2.GetCredibilityInfo("Movies");
+            if (info1.Expertise < info2.Expertise)
+                return 1;
+            else if (info1.Expertise > info2.Expertise)
+                return -1;
+            else
+            {
+                if (info1.RatingCount > info2.RatingCount)
+                    return -1;
+                else if (info1.RatingCount < info2.RatingCount)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+
+        private static void WriteToFile(List<User> l)
+        {
+            TextWriter expFile = new StreamWriter("Movies_Exp.txt");
+            TextWriter trFile = new StreamWriter("Movies_Tr.txt");
+            expFile.WriteLine("user_id\tExpertise\tTrustee_Count");
+            trFile.WriteLine("user_id\tTrustworthiness\tReview_Count");
+
+            l.Sort(userExpComparator);
+            foreach (User u in l)
+            {
+                CredibilityInfo info = u.GetCredibilityInfo("Movies");
+                if(info.RatingCount != 0)
+                    expFile.WriteLine("{0}\t{1}\t{2}", u.Name, 
+                        info.Expertise, info.RatingCount);
+
+                
+            }
+
+            l.Sort(userTrComparator);
+            foreach (User u in l)
+            {
+                CredibilityInfo info = u.GetCredibilityInfo("Movies");
+                if (info.TrusteeCount != 0)
+                    trFile.WriteLine("{0}\t{1}\t{2}", u.Name,
+                        info.Trustworthiness, info.TrusteeCount);
+            }
+
+            expFile.Close();
+            trFile.Close();
+        }
+
+        private static void InsertIntoTable(User u, String table, SqlConnection connection)
+        {
+            foreach(String category in User.CategoryName)
+            {
+                CredibilityInfo info = u.Credibility[category];
+                if(info != null)
+                {
+                    String query = "insert into [" + table + "] ";
+                    query += "values (" + g_count++ + ", "
+                        + "'" + u.Name + "', "
+                        + "'" + category + "', "
+                        + info.Expertise + ", "
+                        + info.RatingCount + ", "
+                        + info.Trustworthiness + ", "
+                        + info.TrusteeCount + ")"
+                        ;
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static void Insert(SqlDataReader reader,
+           Dictionary<String, User> users)
+        {
+            String user_id = reader["user_id"].ToString();
+            user_id = user_id.ToLower();
+            if (!reader["review_helpfulness"].Equals(DBNull.Value))
+            {
+                int rating = (int)reader["review_helpfulness"];
+                if (rating != -1)
+                {
+                    if (!users.ContainsKey(user_id))
+                    {
+                        User newUser = new User(user_id);
+                        newUser.Update(reader["product_category"].ToString(),
+                            rating);
+                        users.Add(user_id, newUser);
+                    }
+                    else
+                    {
+                        User curUser = users[user_id];
+                        curUser.Update(reader["product_category"].ToString(),
+                            rating);
+                    }
+                }
+            }
+        }
+    }
+    
+}
---
+++ User.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Expertise_Rank
+{
+    class User
+    {
+        class CategoryInfo
+        {
+            public String Name;
+            public int MaxRatingCount;
+
+            public CategoryInfo(String name, int maxRatingCount)
+            {
+                Name = name;
+                MaxRatingCount = maxRatingCount;
+            }
+        }
+
+        Books	1078
+Business & Technology	515
+Cars & Motorsports	485
+Computer Hardware	258
+Education	36
+Electronics	525
+Games	499
+Home and Garden	692
+Hotels & Travel	597
+Kids & Family	1544
+Magazines & Newspapers	240
+Movies	1336
+Music	953
+Musical Instruments	279
+Personal Finance	159
+Pets	214
+Restaurants & Gourmet	1145
+Software	103
+Sports & Outdoors	519
+Wellness & Beauty	908
+
+        static public CategoryInfo[] Catetories = new CategoryInfo[] 
+        {
+            new CategoryInfo("Books", 1078), 
+            new CategoryInfo("Business & Technology", 515),
+            new CategoryInfo("Cars & Motorsports", 485),
+            
+        };
+
+        static public String[] CategoryName = new String[]
+        {
+            "Movies",
+            "Books",
+            "Music",
+            "Wellness & Beauty",
+            "Kids & Family",
+            "Home and Garden",
+            "Hotels & Travel",
+            "Electronics",
+            "Restaurants & Gourmet",
+            "Games",
+            "Computer Hardware",
+            "Sports & Outdoors",
+            "Cars & Motorsports",
+            "Pets",
+            "Business & Technology",
+            "Magazines & Newspapers",
+            "Personal Finance",
+            "Software",
+            "Musical Instruments",
+            "Education",
+        };
+        public String Name = "";
+        public Dictionary<String, CredibilityInfo> Credibility =
+            new Dictionary<string, CredibilityInfo>();
+        public List<String> TrustBy;
+
+        public User(String n)
+        {
+            Name = n;
+            foreach (String category in CategoryName)
+            {
+                Credibility.Add(category, new CredibilityInfo());
+            }
+
+            TrustBy = new List<string>();
+        }
+
+        public void Update(String category, int rating)
+        {
+            if (Credibility.ContainsKey(category))
+            {
+                Credibility[category].RatingCount++;
+                Credibility[category].TotalRating += rating;
+            }
+        }
+
+        public void AddTrustBy(String friend)
+        {
+            TrustBy.Add(friend);
+        }
+
+        public void CalculateExpertise()
+        {
+            foreach (KeyValuePair<String, CredibilityInfo> kvp in Credibility)
+            {
+                CredibilityInfo cr = kvp.Value;
+                if (cr.RatingCount != 0)
+                {
+                    double average = (double)cr.TotalRating / (double)cr.RatingCount;
+                    cr.Expertise = average * f((double)cr.RatingCount);
+                }
+            }
+        }
+
+        public void CalculateTrustwhorthiness()
+        {
+            foreach (KeyValuePair<String, CredibilityInfo> kvp in Credibility)
+            {
+                CredibilityInfo cr = kvp.Value;
+                if(cr.TrusteeCount != 0)
+                {
+                    double average = (double)cr.TotalTrusteeExpertise / (double)cr.TrusteeCount;
+                    cr.Trustworthiness = average * f((double)cr.TrusteeCount);
+                   
+                }
+            }
+
+            //if (Name == "stephen_murray" || Name == "shopaholic_man")
+            //    Console.WriteLine(ToString("Movies"));
+        
+        }
+
+        public CredibilityInfo GetCredibilityInfo(String category)
+        {
+            if (Credibility.ContainsKey(category))
+                return Credibility[category];
+            else
+                return null;
+        }
+
+        static private double f(double n)
+        {
+            if (n > 1500.0d)
+                return 1.0d;
+            else
+            {
+                // BASELINE
+                double en = Math.Pow(Math.E, 0.02d * n);
+                return en / (en + 1);
+
+                // CIRCULAR
+                //return Math.Sqrt(1.0d - Math.Pow(n / 1500.0d - 1, 2.0d));
+
+                // INVERSE
+                //return Math.Pow(5.0d, -(1.0d / 0.06d * n));
+            }
+        }
+
+        public String ToString(String category)
+        {
+            String str = Name;
+            CredibilityInfo cr = Credibility[category];
+            if (cr != null)
+            {
+                str += "\n\tTotalRating: " + cr.TotalRating.ToString()
+                    + "\n\tRatingCount: " + cr.RatingCount.ToString()
+                    + "\n\tTotalTrusteeExp: " + cr.TotalTrusteeExpertise.ToString()
+                    + "\n\tTrusteeCount: " + cr.TrusteeCount.ToString()
+                    + "\n\tExpertise: " + cr.Expertise.ToString()
+                    + "\n\tTrustworthiness: " + cr.Trustworthiness.ToString()
+                    ;
+            }
+            str += "\n";
+            return str;
+        }
+
+       
+    }
+
+    /* Credibility information for a category */
+     class CredibilityInfo
+     {
+         public int TotalRating = 0;
+         public int RatingCount = 0;
+         public double TotalTrusteeExpertise = 0.0d;
+         public int TrusteeCount = 0;
+         public double Expertise = 0.0d;
+         public double Trustworthiness = 0.0d;
+
+         public CredibilityInfo()
+         {
+             TotalRating = 0;
+             RatingCount = 0;
+             Expertise = 0.0d;
+             Trustworthiness = 0.0d;
+         }
+
+         public CredibilityInfo(int totalRating, int ratingCount, double exp)
+         {
+             TotalRating = totalRating;
+             RatingCount = ratingCount;
+             Expertise = exp;
+         }
+     }
+}
